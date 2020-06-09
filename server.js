a85bcf236a62e27c3d2d0309df598e667d717061
luis Sandoval <luissandoval1126@gmail.com>
2020-06-09 15:37:24 -0700

    modified the mongodb connection string

--- server.js
+++ server.js
@@ -10,11 +10,8 @@ app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workItOut", {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-});
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workItOut";
+mongoose.connect(MONGODB_URI);
 
 // routes
 app.use(require("./routes/api"));
