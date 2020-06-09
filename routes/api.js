6ed50b435825cdb9b101c002a7fec0de17aa8ef7
luis Sandoval <luissandoval1126@gmail.com>
2020-06-09 16:36:29 -0700

    cant find route dont use route

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,6 @@
 const Workout = require("../models/workout.js");
 const router = require("express").Router();
+const path = require("path");
 
 router.get("/api/workouts", (req, res) => {
   Workout.find({})
@@ -45,6 +46,14 @@ router.get("/api/workouts/range", (req, res) => {
     .catch((err) => {
       res.status(400).json(err);
     });
+});
+
+router.get("/exercise", function (req, res) {
+  res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
+
+router.get("/stats", function (req, res) {
+  res.sendFile(path.join(__dirname, "../public/stats.html"));
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -22,7 +22,7 @@ mongoose.connect(
 
 // routes
 app.use(require("./routes/api"));
-app.use(require("./routes/html"));
+// app.use(require("./routes/html"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
