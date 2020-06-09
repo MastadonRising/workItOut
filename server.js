caa080651ce8bb2f04eaf46a1b95b54a702ad651
luis Sandoval <luissandoval1126@gmail.com>
2020-06-09 15:50:06 -0700

    mlab connection still dumb

--- server.js
+++ server.js
@@ -10,8 +10,14 @@ app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workItOut";
-mongoose.connect(MONGODB_URI);
+
+mongoose.connect(
+  process.env.MONGODB_URI ||
+    "mongodb://user:password1@ds019633.mlab.com:19633/heroku_5q8r2c7f",
+  {
+    useMongoClient: true,
+  }
+);
 
 // routes
 app.use(require("./routes/api"));
